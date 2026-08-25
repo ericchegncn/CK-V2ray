c5d522904546d11d226d710f5739ea5d70676b07
ericchegncn <ericchegncn@users.noreply.github.com>
2026-08-25 15:07:54 +0800

    fix: rename installed app to CK v2ray

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.v2ray.ang"
         minSdk = 24
         targetSdk = 37
-        versionCode = 746
-        versionName = "2.3.5-ck.2"
+        versionCode = 747
+        versionName = "2.3.5-ck.3"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
